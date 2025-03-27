b66caa0e54bd5e61220e22575ee8c6e4f70a8c1d
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2025-03-27 14:31:04 +0800

    change_03/27_14-31

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GalleryPage from './pages/GalleryPage';
 import BookDetailPage from './pages/BookDetailPage';
+import './pages/GalleryPage.css';
+import './pages/BookDetailPage.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Books Gallery</h1>
         </header>
         <Routes>
           <Route path="/" element={<GalleryPage />} />
