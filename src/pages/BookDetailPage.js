9e9e5d687398265c062d17028b756eaefdd86855
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2025-03-27 14:04:23 +0800

    change_03/27_14-04

--- src/pages/BookDetailPage.js
+++ src/pages/BookDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import booksData from './books.json';
+import './BookDetailPage.css';
 
 const BookDetailPage = () => {
   const { id } = useParams();
--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import booksData from './books.json';
+import './GalleryPage.css';
 
 const GalleryPage = () => {
   const [books, setBooks] = useState([]);
