d6a98849991ae96579d1fe8f5b726b10267b466b
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2025-03-28 14:21:22 +0800

    change_03/28_14-21

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-//import './App.css';
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GalleryPage from './pages/GalleryPage';
 import BookDetailPage from './pages/BookDetailPage';
 import './pages/GalleryPage.css';
 import './pages/BookDetailPage.css';
-import booksData from './components/books.json';
 
 function App() {
   return (
